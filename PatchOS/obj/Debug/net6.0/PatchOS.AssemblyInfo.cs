8d211e71287d73981349dd71aa5f9e12fc2f9a19
GeorgeFence <jiri.ploc@email.cz>
2024-01-30 18:28:27 +0100

    update

--- PatchOS/obj/Debug/net6.0/PatchOS.AssemblyInfo.cs
+++ PatchOS/obj/Debug/net6.0/PatchOS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Use VMware Player or Workstation to deploy and debug.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dc63f9f2b6c9a886f95aa05f1b8744e81a96dff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatchOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatchOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
